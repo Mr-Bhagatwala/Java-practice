f1418648c1b4a4b2b6d7ca4e7f92022c8e9d4449
Mr-Bhagatwala <154253285+Mr-Bhagatwala@users.noreply.github.com>
2024-07-23 13:38:35 +0530

    Guess the number

---
+++ src/Inheritance.java
@@ -0,0 +1,28 @@
+
+
+import java.util.Scanner;
+public class Inheritance {
+    public static void main(String[] args) {
+        int myNumber = (int)(Math.random()*100);
+        int userNumber = 0;
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.print("Guess the number between 1 to 100 : ");
+            userNumber = sc.nextInt();
+
+            if(userNumber == myNumber){
+                System.out.println("hohoo ... you guess the right");
+                break;
+            }
+            else if(userNumber > myNumber){
+                System.out.println("Your number is too large...");
+            }else{
+                System.out.println("Your number is too small");
+            }
+        }while(userNumber > 0);
+
+        System.out.print("My number is : ");
+        System.out.println(myNumber);
+
+    }
+}
